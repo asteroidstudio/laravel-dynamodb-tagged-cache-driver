823d0bc7540df5243f61a7d5a2315750dc6a74ea
David Fariña <davefv@gmail.com>
2022-01-22 01:30:20 +0100

    TestCase chore

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,15 +11,6 @@ class TestCase extends Orchestra
 {
     protected $loadEnvironmentVariables = true;
     
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'AsteroidStudio\\LaravelDynamodbTaggedCacheDriver\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
@@ -33,11 +24,7 @@ class TestCase extends Orchestra
         $app->bootstrapWith([LoadEnvironmentVariables::class]);
         parent::getEnvironmentSetUp($app);
         config()->set('database.default', 'testing');
-        config()->set('cache.stores.dynamodb.attributes.sort_key', 'sk');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-dynamodb-tagged-cache-driver_table.php.stub';
-        $migration->up();
-        */
+        config()->set('cache.stores.dynamodb.attributes.sort_key', 'SK');
+        config()->set('cache.stores.dynamodb.attributes.key', 'PK');
     }
 }
